2022f27348ac1d199265abdc89413b4e7865435d
Enzo QUELENIS <equelenis@myges.fr>
2024-10-21 20:07:43 +0200

    fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="fr" className="h-full antialiased" suppressHydrationWarning>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
-          <div className="flex w-full">
+          <div className="flex w-full z-50">
             <Layout>{children}</Layout>
           </div>
         </Providers>
